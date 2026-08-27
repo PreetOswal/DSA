06c75336cf0ea824e4ec01a2f5bc6a5236256e5c
Preet Oswal <preetoswal4747@gmail.com>
2026-08-28 02:48:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (96.58%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.cpp
+++ 2556-convert-the-temperature/convert-the-temperature.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
-        double kelvin = celsius + 273.15;
-        double Fahrenhiet = (celsius * 1.80) +32.00;
+        // double kelvin = ;
+        // double Fahrenhiet = ;
 
-        return {kelvin, Fahrenhiet};
+        return {celsius + 273.15, (celsius * 1.80) +32.00};
     }
 };
